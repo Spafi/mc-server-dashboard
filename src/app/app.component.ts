76559f8fa5aff3ebe61058974c282f57e1f6bbfa
Spafi <63115231+Spafi@users.noreply.github.com>
2022-11-17 20:55:04 +0200

    initial structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+@Component( {
+                selector   : 'app-root',
+                templateUrl: './app.component.html',
+                styleUrls  : [ './app.component.scss' ]
+            } )
 export class AppComponent {
-  title = 'mc-server-dashboard';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputNumberModule } from "primeng/inputnumber";
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+@NgModule( {
+               declarations: [
+                   AppComponent
+               ],
+               imports: [
+                   BrowserModule,
+                   AppRoutingModule,
+                   InputNumberModule,
+                   ButtonModule
+               ],
+               providers   : [],
+               bootstrap   : [ AppComponent ]
+           } )
+export class AppModule {
+}
